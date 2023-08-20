2fb13f3d1b670b35e4011ac481ce2fa006b5ccab
h70076222 <142376079+h70076222@users.noreply.github.com>
2023-08-21 01:41:20 +0800

    Update board.mk

--- trunk/configs/boards/K2P-USB/board.mk
+++ trunk/configs/boards/K2P-USB/board.mk
@@ -4,6 +4,6 @@
 # K2P       # K2P              # MT7621  #
 ##################################################################
 
-CFLAGS += -DBOARD_K2P -DBOARD_MT7615_DBDC
+CFLAGS += -DBOARD_K2P -DBOARD_MT7603_DBDC
 BOARD_NUM_USB_PORTS=1
 CONFIG_BOARD_RAM_SIZE=128
